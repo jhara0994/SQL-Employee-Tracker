f364ab9f1c032adac7af13bd2bfc10131532437a
Jared Haralson <jharalson29@gmail.com>
2022-02-01 01:44:52 -0500

    Updating seeds.sql with manager ID's.

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,14 +11,14 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
     ('Sales Manager', 100000, 1),
-    ('Salesperson', 50000, 2),
-    ('Engineering Manager', 125000, 3),
-    ('Software Engineer', 100000, 4),
-    ('Accounting Manager', 95000, 5),
-    ('Accountant', 60000, 6),
-    ('Lead Lawyer', 200000, 5),
+    ('Salesperson', 50000, 1),
+    ('Engineering Manager', 125000, 2),
+    ('Software Engineer', 100000, 2),
+    ('Accounting Manager', 95000, 3),
+    ('Accountant', 60000, 3),
+    ('Lead Lawyer', 200000, 4),
     ('Paralegal', 50000, 4),
-    ('Certified Ethical Hacker', 200000, 3),
+    ('Certified Ethical Hacker', 200000, 5),
     ('Chief Executive Officer', 250000, 6),
     ('Chief Financial Officer', 175000, 6),
     ('Chief Operating Officer', 150000, 6),
@@ -27,20 +27,20 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Bruce', 'Wayne', 10, NULL),
-    ('Wanda', 'Maximoff', 11, NULL), 
-    ('Maria', 'Rambeau', 12, NULL), 
-    ('Tony', 'Stark', 7, NULL), 
+    ('Bruce', 'Wayne', 6, NULL),
+    ('Wanda', 'Maximoff', 6, 1), 
+    ('Maria', 'Rambeau', 6, 1), 
+    ('Tony', 'Stark', 6, 1), 
     ('Pepper', 'Potts', 3, 4), 
-    ('Natasha', 'Romanoff', 1, NULL), 
-    ('Matt', 'Murdock', 7, NULL), 
-    ('Mary Jane', 'Watson', 5, NULL), 
-    ('Peter', 'Parker', 4, NULL), 
-    ('Daisy', 'Johnson', 9, NULL), 
-    ('Wade', 'Wilson', 2, NULL), 
-    ('Robert', 'Drake', 6, NULL), 
-    ('Loki', 'Laufeyson', 8, NULL),
-    ('Carol', 'Danvers', 4, NULL)
+    ('Natasha', 'Romanoff', 2, 3), 
+    ('Matt', 'Murdock', 1, 1), 
+    ('Mary Jane', 'Watson', 4, 2), 
+    ('Peter', 'Parker', 3, 5), 
+    ('Daisy', 'Johnson', 5, 4), 
+    ('Wade', 'Wilson', 1, 6), 
+    ('Robert', 'Drake', 3, 8), 
+    ('Loki', 'Laufeyson', 4, 7),
+    ('Carol', 'Danvers', 2, 5)
 
 
 
